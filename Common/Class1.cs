9747e21d7957e647f03a9af85215306f1f5a5e23
Pavel <black_yaguar@mail.ru>
2022-12-14 19:23:13 +0500

    создание библиотеки

--- Common/Class1.cs
+++ Common/Class1.cs
@@ -1,5 +1,15 @@
 ﻿namespace Common;
-public class Class1
+public static class Class1
 {
 
+    static int [] PrintArr(int [] array)
+    {
+        Console.Write("[");
+        for(int i=0; i<array.Length;i++)
+        {
+            Console.Write($"{array[i]},");
+        }
+        Console.Write("]");
+    return (array);
+}
 }
