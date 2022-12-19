a2697aca33dae9a3a8df20fb0530691b3c686b36
Pavel <black_yaguar@mail.ru>
2022-12-19 20:28:06 +0500

    Первая задача седьмой домашний работы.

--- Homework7/Task1/Program.cs
+++ Homework7/Task1/Program.cs
@@ -7,17 +7,23 @@
 // 1 -3,3 8 -9,9
 
 // 8 7,8 -7,1 9
+Method();
 
-
-
-
-// int[] array = new int[3] { 1, 3, 4 };
-
-// string str = string.Join(", ", array);
-// Console.WriteLine(str);
-
-double [,] array = CreateRandom2DArray(3,4);
+int i = InputNumber();
+int j = InputNumber();
+double [,] array = CreateRandom2DArray(i,j);
 Print2DArray(array);
+
+
+
+
+
+void Method()
+{
+  Console.WriteLine("Задайте двумерный массив размером m на n.");
+ Console.WriteLine("Заполненный случайными вещественными числами.");
+}
+
 
 
 void Print2DArray(double [,] array){
@@ -30,12 +36,28 @@ void Print2DArray(double [,] array){
 }
 double [,] CreateRandom2DArray(double  countRows, double  countColums){
     Random random = new Random();
-   double [,] array = new double [countRows,countColums];
+   double [,] array = new double [i,j];
     for(int i=0; i<array.GetLength(0); i++){
-        for(int j=0; j<array.GetLength(1); j++){
-            array[i,j] = random.NextDouble();
+        for(int j=0; j<array.GetLength(1); j++)
+        {
+            array[i,j] = Math.Round((random.NextDouble()-0.4)*10,1);
         }
    
     }
-    return (double array);
+    return ( array);
 }
+int InputNumber()
+{
+    Console.Write("Введите  число ");
+    while (!int.TryParse(Console.ReadLine(), out j))
+    {
+        Console.Write("Ошибка ввода! Введите целое число ");
+    }
+    return(j);
+}
+
+
+// int[] array = new int[3] { 1, 3, 4 };
+
+// string str = string.Join(", ", array);
+// Console.WriteLine(str);
