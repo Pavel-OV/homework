10324f5265da98086cfef4d28a907cbc0ae87cc8
Pavel <black_yaguar@mail.ru>
2022-12-22 22:39:18 +0500

    Первая задача

--- Homework8/Task1/Program.cs
+++ Homework8/Task1/Program.cs
@@ -1,26 +1,4 @@
 ﻿
-// Задача 58: Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
-// Например, даны 2 матрицы:
-// 2 4 | 3 4
-// 3 2 | 3 3
-// Результирующая матрица будет:
-// 18 20
-// 15 18
-
-// Задача 60. ...Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
-// Массив размером 2 x 2 x 2
-// 66(0,0,0) 25(0,1,0)
-// 34(1,0,0) 41(1,1,0)
-// 27(0,0,1) 90(0,1,1)
-// 26(1,0,1) 55(1,1,1)
-
-// Задача 62. Напишите программу, которая заполнит спирально массив 4 на 4.
-// Например, на выходе получается вот такой массив:
-// 01 02 03 04
-// 12 13 14 05
-// 11 16 15 06
-// 10 09 08 07
-
 // Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
 // Например, задан массив:
 // 1 4 7 2
@@ -34,11 +12,11 @@ int columsNumber= InputNumber();
 int rowsNumber = InputNumber();
 int[,] array = CreateRandom2DArray(rowsNumber,columsNumber);
 Print2DArray(array);
+Console.WriteLine();
   int[] row = new int[columsNumber];
   for(int i=0; i<rowsNumber; i++){
     for(int j=0; j<columsNumber;j++)
     row[j] =array[i,j];
-   
     BubbleSort(row);
     Insert(true,i,row,array);
   }
@@ -57,30 +35,6 @@ Print2DArray(array);
              dest[k, dim] = source[k];
      }
  }
-// int[] arrayCopied =  ArrayCopy(array);
-
-
-// int[] ArrayCopy(int[] array)
-// {
-//     int[] arrayCopied= new int[array.GetLength(0)];
-//     for(int i=0; i<array.GetLength(0); i++)
-//     {
-//         arrayCopied[i]=array[i];
-//     }
-//     return arrayCopied;
-// }
-
-// for (int i = 0; i < array.Length; i++)
-//     for (int j = 0; j < array.Length - 1; j++)
-//      if (array[j] < array[j + 1])
-//      {
-//       int t = array[j];
-//       array[j] = array[j+1];
-//       array[j+1] = t;
-//      }
-
-
-//Print2DArray(arrayCopied);
 
 int [,] CreateRandom2DArray(int countRows, int  countColums)
     {
