c71fce986a069e52763073f62b0bd2bf3dcbfed8
Pavel <black_yaguar@mail.ru>
2022-12-26 19:20:28 +0500

    дооформление задач, под чистовик

--- Homework9/Task1/Program.cs
+++ Homework9/Task1/Program.cs
@@ -6,22 +6,6 @@
 // N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
 
 
-// Methood();
-// void Methood(int i = 10)
-// {
-   
-//     if(i==1)
-//     {
-//         return;
-//     }
-//     Console.Write($"{i}=");
-//     Methood(i-1);
-// }
-
-// void Methood(int [] arr =null)
-// {
-
-// }
 Console.WriteLine("Задайте значение N.");
 Console.WriteLine("Bыведеm все натуральные числа в промежутке от N до 1.");
 int number = InputNumber();
@@ -40,10 +24,7 @@ void Methood(int number)
 }
 
 
-// bool isParsed = int.TryParse(Console.ReadLine(), out int day);
-// if(!isParsed){
-//     Console.WriteLine("Вы ввели буквы или что-то ещё, надо числа");
-// }
+
 
 int InputNumber()
 {
--- Homework9/Task3/Program.cs
+++ Homework9/Task3/Program.cs
@@ -1,7 +1,7 @@
 ﻿// // Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
 // // m = 2, n = 3 -> A(m,n) = 9
 // // m = 3, n = 2 -> A(m,n) = 29
-
+Console.WriteLine("Bычислениre функции Аккермана с помощью рекурсии");
 int numberM= InputNumber();
 Console.WriteLine($"M->{numberM}");
 int numberN = InputNumber();
@@ -36,20 +36,7 @@ int InputNumber()
     }
     return(numberN);
 }
-// Console.WriteLine("Введите начальное число M:");
-// int numberM = int.Parse(Console.ReadLine());
 
-// Console.WriteLine("Введите начальное число N:");
-// int numberN = int.Parse(Console.ReadLine());
-
-// ///Метод вычисления функции Аккермана:
-// int AckermannFunction (int numberM, int numberN)
-// {
-//     if (numberM == 0) return numberN + 1;
-//     if (numberM != 0 && numberN == 0) return AckermannFunction(numberM - 1, 1);
-//     if (numberM > 0 && numberN > 0) return AckermannFunction(numberM - 1, AckermannFunction(numberM, numberN - 1));
-// return AckermannFunction(numberM, numberN);
-// }
 void Methood()
 {
     Console.WriteLine($"Функция Аккермана для чисел A({numberM},{numberN}) = {GetAckermannFunction(numberM, numberN)}");
