979ad3dab0afcd70e7f8a276dcac31e0fd067280
Pavel <black_yaguar@mail.ru>
2023-01-05 20:03:13 +0500

    Пятая задача

--- Homework8/Task6/Program.cs
+++ Homework8/Task6/Program.cs
@@ -5,11 +5,11 @@
 // 11 16 15 06
 // 10 09 08 07
 
-// int rowsNumber = InputNumber();
-// int columsNumber = rowsNumber;
+int rowsNumber = InputNumber();
+int columsNumber = rowsNumber;
 //int [,] array = CreateRandom2DArray(rowsNumber,columsNumber);
-int n=4;
-int [,] zmey = new int[n,n];
+
+int[,] zmey = new int[rowsNumber, columsNumber];
 int number = 1;
 int rows = 0;
 int colums = 0;
@@ -18,15 +18,16 @@ int colums = 0;
 while (number <= zmey.GetLength(0) * zmey.GetLongLength(1))
 {
     zmey[rows, colums] = number;
-    if (rows <= colums+1 && rows + colums < zmey.GetLength(1) - 1)
+
+    if (rows <= colums + 1 && rows + colums < zmey.GetLength(1) - 1)
     {
         colums = colums + 1;
     }
-    else if (rows < colums && rows + colums < zmey.GetLength(0) - 1)
+    else if (rows < colums && rows + colums >=zmey.GetLength(0) - 1)
     {
         rows = rows + 1;
     }
-    else if (rows >= colums && rows + colums < zmey.GetLength(1) - 1)
+    else if (rows >= colums && rows + colums > zmey.GetLength(1) - 1)
     {
         colums = colums - 1;
     }
@@ -38,6 +39,7 @@ while (number <= zmey.GetLength(0) * zmey.GetLongLength(1))
 
 
     number = number + 1;
+
 }
 
 Print2DArray(zmey);
@@ -47,6 +49,11 @@ void Print2DArray(int[,] array)
     for (int i = 0; i < array.GetLength(0); i++)
     {
         for (int j = 0; j < array.GetLength(1); j++)
+        if (array[i,j]<10)
+        {
+            Console.Write($"0{array[i, j]} ");
+        }
+        else
         {
             Console.Write($"{array[i, j]} ");
         }
@@ -58,12 +65,12 @@ void Print2DArray(int[,] array)
 
 
 
-// int InputNumber()
-// {
-//     Console.Write("Введите  число ");
-//     while (!int.TryParse(Console.ReadLine(), out rowsNumber))
-//     {
-//         Console.Write("Ошибка ввода! Введите целое число ");
-//     }
-//     return (rowsNumber);
-// }
+int InputNumber()
+{
+    Console.Write("Введите  число ");
+    while (!int.TryParse(Console.ReadLine(), out rowsNumber))
+    {
+        Console.Write("Ошибка ввода! Введите целое число ");
+    }
+    return (rowsNumber);
+}
