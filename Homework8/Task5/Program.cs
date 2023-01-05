96e3413892a2a562dc528095d5e63be44e0d3b38
Pavel <black_yaguar@mail.ru>
2023-01-05 15:54:40 +0500

    дз

--- Homework8/Task5/Program.cs
+++ Homework8/Task5/Program.cs
@@ -5,47 +5,62 @@
 // 11 16 15 06
 // 10 09 08 07
 
-int rowsNumber= InputNumber();
-int columsNumber=rowsNumber;
+int rowsNumber = InputNumber();
+int columsNumber = rowsNumber;
 //int [,] array = CreateRandom2DArray(rowsNumber,columsNumber);
-int[,] spiral = new int[rowsNumber,columsNumber];
+int[,] spiral = new int[rowsNumber, columsNumber];
 
 int number = 1;
-int rows =0;
-int colums =0;
+int rows = 0;
+int colums = 0;
 
 
-while (number <= spiral.GetLength(0)*spiral.GetLongLength(1))
+while (number <= spiral.GetLength(0) * spiral.GetLongLength(1))
 {
-    spiral[rows,colums]=number;
-    if (rows<colums && rows+colums<spiral.GetLength(0)-1) colums ++;
-    if
+    spiral[rows, colums] = number;
+    if (rows < colums && rows + colums < spiral.GetLength(0) - 1) colums++;
+    else if (rows >= colums && rows + colums < spiral.GetLength(1) - 1)
+    {
+        rows = rows + 1;
+    }
+     else if (rows >= colums && rows + colums < spiral.GetLength(0) - 1)
+    {
+        colums--;
+    }
+    else
+
+    {
+        rows = rows - 1;
+    }
+
+
+    number = number + 1;
 }
 
 Print2DArray(spiral);
 
-void Print2DArray(int [,] array)
+void Print2DArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
     {
-        for(int i=0; i<array.GetLength(0); i++)
+        for (int j = 0; j < array.GetLength(1); j++)
         {
-            for(int j=0; j<array.GetLength(1); j++)
-            {
-            Console.Write($"{array[ i, j]} ");
-            }
-         Console.WriteLine();
+            Console.Write($"{array[i, j]} ");
         }
+        Console.WriteLine();
     }
+}
 
 
 
 
 
-  int InputNumber()
+int InputNumber()
 {
     Console.Write("Введите  число ");
     while (!int.TryParse(Console.ReadLine(), out rowsNumber))
     {
         Console.Write("Ошибка ввода! Введите целое число ");
     }
-    return(rowsNumber);
+    return (rowsNumber);
 }
